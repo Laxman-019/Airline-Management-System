9a2de35d05205617ceb7e753efd97cb49ff48bcd
Laxman Kumar Chourasia <22cse029@abit.edu.in>
2025-08-28 10:48:55 +0530

    Update Conn.java

--- Airline Management System/src/airline/management/system/Conn.java
+++ Airline Management System/src/airline/management/system/Conn.java
@@ -10,10 +10,11 @@ public class Conn {
     public Conn() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            c = DriverManager.getConnection("jdbc:mysql:///airlinemanagementsystem", "root", "laxman");
+            c = DriverManager.getConnection("jdbc:mysql:///airlinemanagementsystem", "root", "******");
             s = c.createStatement();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
+
